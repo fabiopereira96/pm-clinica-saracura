7a8e5d9d28c7cf6e3e44194860572b0e2bf2163c
matheushsouza <matheus.hs97@gmail.com>
2018-12-08 23:05:57 -0200

    Criado o design da interface de agendamento de consultas.

--- src/br/com/pm/clinicasaracura/AgendamentoWindow.java
+++ src/br/com/pm/clinicasaracura/AgendamentoWindow.java
@@ -5,10 +5,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import com.toedter.calendar.JDayChooser;
+import com.toedter.calendar.JCalendar;
+import javax.swing.JList;
+import java.awt.Font;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.ButtonGroup;
+import javax.swing.JToolBar;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class AgendamentoWindow {
 
 	private JFrame frame;
+	private JTextField nomePacienteTxtField;
+	private JTextField telefoneTxtField;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JTextField matriculaTxtField;
 
 	/**
 	 * Launch the application.
@@ -39,8 +56,119 @@ public class AgendamentoWindow {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setResizable(false);
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 536, 381);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		JPanel particularPanel = new JPanel();
+		particularPanel.setVisible(false);
+		particularPanel.setBounds(247, 184, 273, 155);
+		frame.getContentPane().add(particularPanel);
+		particularPanel.setLayout(null);
+		
+		JLabel lblNewLabel_6 = new JLabel("Forma de pagamento");
+		lblNewLabel_6.setFont(new Font("Dialog", Font.BOLD, 11));
+		lblNewLabel_6.setBounds(12, 12, 249, 15);
+		particularPanel.add(lblNewLabel_6);
+		
+		JRadioButton chequeRadio = new JRadioButton("Cheque");
+		chequeRadio.setFont(new Font("Dialog", Font.BOLD, 11));
+		chequeRadio.setBounds(8, 35, 149, 23);
+		particularPanel.add(chequeRadio);
+		
+		JRadioButton creditoRadio = new JRadioButton("Crédito");
+		creditoRadio.setFont(new Font("Dialog", Font.BOLD, 11));
+		creditoRadio.setBounds(8, 62, 149, 23);
+		particularPanel.add(creditoRadio);
+		
+		JRadioButton debitoRadio = new JRadioButton("Débito");
+		debitoRadio.setFont(new Font("Dialog", Font.BOLD, 11));
+		debitoRadio.setBounds(8, 89, 149, 23);
+		particularPanel.add(debitoRadio);
+		
+		JRadioButton dinheiroRadio = new JRadioButton("Dinheiro");
+		dinheiroRadio.setFont(new Font("Dialog", Font.BOLD, 11));
+		dinheiroRadio.setBounds(8, 116, 149, 23);
+		particularPanel.add(dinheiroRadio);
+		
+		JPanel convenioPanel = new JPanel();
+		convenioPanel.setBounds(247, 184, 273, 155);
+		frame.getContentPane().add(convenioPanel);
+		convenioPanel.setLayout(null);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(12, 30, 249, 55);
+		convenioPanel.add(scrollPane_1);
+		
+		JList conveniosList = new JList();
+		scrollPane_1.setColumnHeaderView(conveniosList);
+		
+		JLabel lblNewLabel_4 = new JLabel("Selecione o convenio");
+		lblNewLabel_4.setFont(new Font("Dialog", Font.BOLD, 11));
+		lblNewLabel_4.setBounds(12, 12, 249, 15);
+		convenioPanel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Matricula do cliente");
+		lblNewLabel_5.setFont(new Font("Dialog", Font.BOLD, 11));
+		lblNewLabel_5.setBounds(12, 92, 249, 15);
+		convenioPanel.add(lblNewLabel_5);
+		
+		matriculaTxtField = new JTextField();
+		matriculaTxtField.setBounds(12, 110, 249, 19);
+		convenioPanel.add(matriculaTxtField);
+		matriculaTxtField.setColumns(10);
+		
+		JCalendar calendar = new JCalendar();
+		calendar.setBounds(12, 34, 223, 138);
+		frame.getContentPane().add(calendar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 184, 223, 155);
+		frame.getContentPane().add(scrollPane);
+		
+		JList horariosList = new JList();
+		scrollPane.setViewportView(horariosList);
+		
+		JLabel lblNewLabel = new JLabel("Selecione uma data e um horário");
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 11));
+		lblNewLabel.setBounds(12, 12, 223, 25);
+		frame.getContentPane().add(lblNewLabel);
+		
+		nomePacienteTxtField = new JTextField();
+		nomePacienteTxtField.setBounds(247, 34, 273, 19);
+		frame.getContentPane().add(nomePacienteTxtField);
+		nomePacienteTxtField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nome do paciente");
+		lblNewLabel_1.setFont(new Font("Dialog", Font.BOLD, 11));
+		lblNewLabel_1.setBounds(247, 16, 187, 15);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Telefone do paciente");
+		lblNewLabel_2.setFont(new Font("Dialog", Font.BOLD, 11));
+		lblNewLabel_2.setBounds(247, 67, 187, 15);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		telefoneTxtField = new JTextField();
+		telefoneTxtField.setBounds(247, 85, 273, 19);
+		frame.getContentPane().add(telefoneTxtField);
+		telefoneTxtField.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Forma de atendimento");
+		lblNewLabel_3.setFont(new Font("Dialog", Font.BOLD, 11));
+		lblNewLabel_3.setBounds(247, 137, 187, 15);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JRadioButton convenioRadio = new JRadioButton("Convênio");
+		convenioRadio.setFont(new Font("Dialog", Font.BOLD, 11));
+		buttonGroup.add(convenioRadio);
+		convenioRadio.setBounds(243, 160, 90, 23);
+		frame.getContentPane().add(convenioRadio);
+		
+		JRadioButton particularRadio = new JRadioButton("Particular");
+		particularRadio.setFont(new Font("Dialog", Font.BOLD, 11));
+		buttonGroup.add(particularRadio);
+		particularRadio.setBounds(375, 160, 149, 23);
+		frame.getContentPane().add(particularRadio);
 	}
 }
